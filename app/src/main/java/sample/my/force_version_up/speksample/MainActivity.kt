4c6244fa18363b79c33c479e93b2f935623e1cbe
shinya-takano <dev.shinya.takano@gmail.com>
2020-05-31 11:29:02 +0900

    Initial commit Create Fragment + ViewModel Project by Android Studio

---
+++ app/src/main/java/sample/my/force_version_up/speksample/MainActivity.kt
@@ -0,0 +1,18 @@
+package sample.my.force_version_up.speksample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import sample.my.force_version_up.speksample.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/sample/my/force_version_up/speksample/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package sample.my.force_version_up.speksample.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
